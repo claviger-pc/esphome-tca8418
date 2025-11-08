b1390c5604c2d46b47e4459f479401ffefbf0362
Petrus Claviger <claviger-pc@users.noreply.github.com>
2025-11-08 15:12:51 +0100

    Refactor TCA8418 binary sensor configuration

--- components/tca8418/binary_sensor.py
+++ components/tca8418/binary_sensor.py
@@ -1,4 +1,5 @@
 # components/tca8418/binary_sensor.py
+
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import binary_sensor
@@ -9,13 +10,7 @@ from . import tca8418_ns, TCA8418Component
 CONF_TCA8418_ID = "tca8418_id"
 CONF_KEY = "key"
 
-TCA8418BinarySensor = tca8418_ns.class_(
-    "TCA8418BinarySensor",
-    binary_sensor.BinarySensor,
-    cg.Parented(TCA8418Component),
-)
-
-CONFIG_SCHEMA = binary_sensor.binary_sensor_schema(TCA8418BinarySensor).extend(
+CONFIG_SCHEMA = binary_sensor.binary_sensor_schema().extend(
     {
         cv.GenerateID(CONF_TCA8418_ID): cv.use_id(TCA8418Component),
         cv.Required(CONF_KEY): cv.int_range(min=0, max=63),
@@ -27,9 +22,5 @@ async def to_code(config):
     hub = await cg.get_variable(config[CONF_TCA8418_ID])
     var = await binary_sensor.new_binary_sensor(config)
 
-    # connect child -> parent
-    cg.add(var.set_parent(hub))
-    cg.add(var.set_key_index(config[CONF_KEY]))
-
-    # register this key with the hub
-    cg.add(hub.register_key_sensor(config[CONF_KEY], var))
+    key = config[CONF_KEY]
+    cg.add(hub.register_key_sensor(key, var))
