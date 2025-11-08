19d21df60678a070d1da7e8919edf9f4840b2e0a
Petrus Claviger <claviger-pc@users.noreply.github.com>
2025-11-08 15:05:34 +0100

    Refactor TCA8418Component configuration schema
    Removed rows and cols configuration options from TCA8418Component.

--- components/tca8418/__init__.py
+++ components/tca8418/__init__.py
@@ -1,23 +1,20 @@
 # components/tca8418/__init__.py
+
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import i2c, gpio
-from esphome.const import CONF_ID
+from esphome.components import i2c
+from esphome import pins
 
 tca8418_ns = cg.esphome_ns.namespace("tca8418")
 TCA8418Component = tca8418_ns.class_("TCA8418Component", cg.Component, i2c.I2CDevice)
 
-CONF_ROWS = "rows"
-CONF_COLS = "cols"
 CONF_INTERRUPT_PIN = "interrupt_pin"
 
 CONFIG_SCHEMA = (
     i2c.i2c_device_schema(TCA8418Component)
     .extend(
         {
-            cv.Optional(CONF_ROWS, default=6): cv.int_range(min=1, max=8),
-            cv.Optional(CONF_COLS, default=4): cv.int_range(min=1, max=10),
-            cv.Optional(CONF_INTERRUPT_PIN): cv.internal_gpio_input_pin_schema,
+            cv.Optional(CONF_INTERRUPT_PIN): pins.internal_gpio_input_pin_schema,
         }
     )
 )
@@ -25,9 +22,6 @@ CONFIG_SCHEMA = (
 
 async def to_code(config):
     var = await i2c.register_i2c_device(config, TCA8418Component)
-
-    cg.add(var.set_rows(config[CONF_ROWS]))
-    cg.add(var.set_cols(config[CONF_COLS]))
 
     if CONF_INTERRUPT_PIN in config:
         pin = await cg.gpio_pin_expression(config[CONF_INTERRUPT_PIN])
