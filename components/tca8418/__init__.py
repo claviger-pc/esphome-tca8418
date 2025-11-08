15ade97c112a37a82c5ab4651876f235d443b5ab
Petrus Claviger <claviger-pc@users.noreply.github.com>
2025-11-08 15:23:43 +0100

    Refactor CONFIG_SCHEMA and to_code function

--- components/tca8418/__init__.py
+++ components/tca8418/__init__.py
@@ -6,26 +6,31 @@ from esphome.components import i2c
 from esphome import pins
 from esphome.const import CONF_ID
 
+DEPENDENCIES = ["i2c"]
+
 tca8418_ns = cg.esphome_ns.namespace("tca8418")
 TCA8418Component = tca8418_ns.class_("TCA8418Component", cg.Component, i2c.I2CDevice)
 
 CONF_INTERRUPT_PIN = "interrupt_pin"
 
-CONFIG_SCHEMA = (
-    i2c.i2c_device_schema(TCA8418Component)
-    .extend(
-        {
-            cv.GenerateID(): cv.declare_id(TCA8418Component),
-            cv.Optional(CONF_INTERRUPT_PIN): pins.internal_gpio_input_pin_schema,
-        }
-    )
-)
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(TCA8418Component),
+        cv.Optional(CONF_INTERRUPT_PIN): pins.internal_gpio_input_pin_schema,
+    }
+).extend(i2c.i2c_device_schema(0x34))
+
 
 async def to_code(config):
-    var = await i2c.register_i2c_device(config, TCA8418Component)
+    var = cg.new_Pvariable(config[CONF_ID])
 
+    # Register as a normal component
+    await cg.register_component(var, config)
+
+    # Register as an I2C device (handles address, i2c_id, etc.)
+    await i2c.register_i2c_device(var, config)
+
+    # Optional interrupt pin
     if CONF_INTERRUPT_PIN in config:
         pin = await cg.gpio_pin_expression(config[CONF_INTERRUPT_PIN])
         cg.add(var.set_interrupt_pin(pin))
-
-    cg.add_global(cg.App.register_component(var))
