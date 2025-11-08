2865136b62fc0b8007cdde9cd6811dcb2075b48a
Petrus Claviger <claviger-pc@users.noreply.github.com>
2025-11-08 15:13:09 +0100

    Refactor TCA8418 to use binary_sensor interface

--- components/tca8418/tca8418.h
+++ components/tca8418/tca8418.h
@@ -10,15 +10,11 @@
 namespace esphome {
 namespace tca8418 {
 
-class TCA8418BinarySensor;
-
 class TCA8418Component : public Component, public i2c::I2CDevice {
  public:
-  void set_rows(uint8_t rows) { rows_ = rows; }
-  void set_cols(uint8_t cols) { cols_ = cols; }
   void set_interrupt_pin(InternalGPIOPin *pin) { interrupt_pin_ = pin; }
 
-  void register_key_sensor(uint8_t index, TCA8418BinarySensor *sensor);
+  void register_key_sensor(uint8_t index, binary_sensor::BinarySensor *sensor);
 
   void setup() override;
   void loop() override;
@@ -28,26 +24,17 @@ class TCA8418Component : public Component, public i2c::I2CDevice {
  protected:
   static const uint8_t MAX_KEYS = 64;
 
-  TCA8418BinarySensor *key_sensors_[MAX_KEYS] = {nullptr};
+  binary_sensor::BinarySensor *key_sensors_[MAX_KEYS] = {nullptr};
 
-  uint8_t rows_{0};
-  uint8_t cols_{0};
+  // For now we assume a fixed 6x4 matrix (0..23) for OMOTE;
+  // you can make this configurable later if you want.
+  uint8_t rows_{6};
+  uint8_t cols_{4};
   InternalGPIOPin *interrupt_pin_{nullptr};
   uint32_t last_poll_{0};
 
   bool read_key_event_(uint8_t &key_event);
   void process_key_event_(uint8_t key_event);
-};
-
-class TCA8418BinarySensor : public binary_sensor::BinarySensor,
-                            public Parented<TCA8418Component> {
- public:
-  void set_key_index(uint8_t key) { key_index_ = key; }
-  uint8_t get_key_index() const { return key_index_; }
-
- protected:
-  friend class TCA8418Component;
-  uint8_t key_index_{0};
 };
 
 }  // namespace tca8418
