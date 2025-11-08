78d97a68ff388b7b94c9b662d8922b64db2650d7
Petrus Claviger <claviger-pc@users.noreply.github.com>
2025-11-08 14:42:02 +0100

    Refactor TCA8418 component configuration and schema

--- components/tca8418/__init__.py
+++ components/tca8418/__init__.py
@@ -1,39 +1,34 @@
+# components/tca8418/__init__.py
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome import pins
-from esphome.components import i2c, key_provider
-from esphome.const import CONF_ID, CONF_INTERRUPT_PIN
-
-AUTO_LOAD = ["key_provider"]
-
-DEPENDENCIES = ["i2c"]
-MULTI_CONF = True
+from esphome.components import i2c, gpio
+from esphome.const import CONF_ID
 
 tca8418_ns = cg.esphome_ns.namespace("tca8418")
-TCA8418Component = tca8418_ns.class_(
-    "TCA8418Component",
-    key_provider.KeyProvider,
-    cg.Component,
-    i2c.I2CDevice
-)
+TCA8418Component = tca8418_ns.class_("TCA8418Component", cg.Component, i2c.I2CDevice)
 
-CONF_TCA8418_ID = "tca8418_id"
+CONF_ROWS = "rows"
+CONF_COLS = "cols"
+CONF_INTERRUPT_PIN = "interrupt_pin"
 
 CONFIG_SCHEMA = (
-    cv.Schema(
+    i2c.i2c_device_schema(TCA8418Component)
+    .extend(
         {
-            cv.GenerateID(): cv.declare_id(TCA8418Component),
-            cv.Required(CONF_INTERRUPT_PIN): pins.internal_gpio_input_pin_schema
+            cv.Optional(CONF_ROWS, default=6): cv.int_range(min=1, max=8),
+            cv.Optional(CONF_COLS, default=4): cv.int_range(min=1, max=10),
+            cv.Optional(CONF_INTERRUPT_PIN): cv.internal_gpio_input_pin_schema,
         }
     )
-    .extend(cv.COMPONENT_SCHEMA)
-    .extend(i2c.i2c_device_schema(0x34))
 )
 
-async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
-    await i2c.register_i2c_device(var, config)
 
-    interrupt_pin = await cg.gpio_pin_expression(config[CONF_INTERRUPT_PIN])
-    cg.add(var.set_interrupt_pin(interrupt_pin))
+async def to_code(config):
+    var = await i2c.register_i2c_device(config, TCA8418Component)
+
+    cg.add(var.set_rows(config[CONF_ROWS]))
+    cg.add(var.set_cols(config[CONF_COLS]))
+
+    if CONF_INTERRUPT_PIN in config:
+        pin = await cg.gpio_pin_expression(config[CONF_INTERRUPT_PIN])
+        cg.add(var.set_interrupt_pin(pin))
